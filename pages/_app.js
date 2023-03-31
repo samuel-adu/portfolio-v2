ed3b6bedcb626aee806148c5d25004b31d873d64
Samuel Adu <theadusamuel@gmail.com>
2023-03-31 19:03:04 +0100

    add mobile styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import { Public_Sans, Ibarra_Real_Nova } from "next/font/google";
+import "@/styles/globals.css";
+import Layout from "../components/layout";
+
+const public_Sans = Public_Sans({
+  subsets: ["latin"],
+  variable: "--ff-primary",
+});
+
+const ibarra_Real_Nova = Ibarra_Real_Nova({
+  subsets: ["latin"],
+  variable: "--ff-secondary",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${public_Sans.variable} font-sans`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
