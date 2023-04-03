7b0578da7cdfa1271f56ac34f4cca8d745682e6f
Samuel Adu <theadusamuel@gmail.com>
2023-04-03 06:24:03 +0100

    social links to desktop site

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,6 +13,41 @@ export default function Navbar() {
           </Link>
           <span className="font-mono mr-auto">theadusamuel</span>
 
+          <ul className="hidden sm:flex gap-4 items-center mx-4">
+            <li>
+              <a
+                href="https://github.com/theadusamuel"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-github text-2xl"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/theadusamuel"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-linkedin text-2xl"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.frontendmentor.io/profile/theadusamuel"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/images/icons/frontend-mentor.svg"
+                  alt=""
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </li>
+          </ul>
+
           {/* <i className="fa-regular fa-moon text-2xl"></i> */}
           <i className="fa-solid fa-sun text-2xl"></i>
         </div>
