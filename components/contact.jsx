bfe9860c0a25bbb312013e65684ee280ed930e39
Samuel Adu <theadusamuel@gmail.com>
2023-07-19 08:07:28 +0100

    remove the contact form and just use a button  to send email

--- components/contact.jsx
+++ components/contact.jsx
@@ -2,57 +2,20 @@
 export default function Contact() {
   return (
     <>
-      <div>
+      <div className="text-center">
         <h2 className="text-center text-3xl font-semibold">Get in Touch</h2>
         <p className="text-center">
           Do you have a job opportunity or a project I can be a part of? Feel
-          free to reach me at{" "}
-          <span className="text-primary-500">theadusamuel@gmail.com</span>, you
-          may also leave me a message using the form below and I'll get back to
-          you.
+          free to leave me a message and I'll get back to you as soon as
+          possible.
         </p>
-      </div>
 
-      <hr className="my-6 border-t border-solid border-neutral-200 dark:border-neutral-700 sm:hidden" />
-
-      <div>
-        <form className="flex flex-col">
-          <label className="mb-2 block w-full" htmlFor="fullName">
-            Name
-          </label>
-          <input
-            className="mb-6 block h-12 w-full rounded bg-neutral-200 px-6 py-2 placeholder:font-primary placeholder:text-xs"
-            type="text"
-            name="fullName"
-            placeholder="Jane Appleseed"
-          />
-
-          <label className="mb-2 block w-full" htmlFor="email">
-            Email Address
-          </label>
-          <input
-            className="mb-6 block h-12 w-full rounded bg-neutral-200 px-6 py-2 placeholder:font-primary placeholder:text-xs"
-            type="text"
-            name="email"
-            placeholder="email@example.com"
-          />
-
-          <label className="mb-2 block w-full" htmlFor="fullName">
-            Message
-          </label>
-          <textarea
-            className="mb-6 block h-24 w-full resize-none rounded bg-neutral-200 px-6 py-2 placeholder:font-primary placeholder:text-xs"
-            type="text"
-            placeholder="How can I help?"
-          />
-
-          <button
-            className="pointer mb-6 inline-block h-12 w-full rounded bg-primary-500 px-6 py-2 text-xs uppercase tracking-widest text-neutral-100 hover:bg-primary-700 md:ml-auto md:w-48"
-            type="submit"
-          >
-            Send Message
-          </button>
-        </form>
+        <a
+          href="mailto:theadusamuel@gmail.com"
+          className="pointer my-6 inline-block rounded bg-primary-500 px-6 py-4 text-center text-xs capitalize tracking-widest text-neutral-100 hover:bg-primary-700"
+        >
+          Send Email
+        </a>
       </div>
     </>
   );
