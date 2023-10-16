fdc1202418bce3cc03e22626aeaf82531e2ef04f
Samuel Adu <theadusamuel@gmail.com>
2023-10-16 20:09:46 +0100

    reduced number of projects to 3

--- data.js
+++ data.js
@@ -29,35 +29,4 @@ export const projects = [
     liveUrl: "http://advice-generator-app-dun.vercel.app/",
     tools: ["React", "API"],
   },
-  {
-    id: "04",
-    title: "tenzies game",
-    description: "This is a fun dice game that can be played to pass time.",
-    img: "tenzies-game-screenshot.png",
-    github: "https://github.com/theadusamuel/tenzies-game",
-    liveUrl: "https://tenzies-game-beryl.vercel.app",
-    tools: ["javascript"],
-  },
-
-  {
-    id: "05",
-    title: "card details form",
-    description:
-      "This fun project was an excellent way to practice DOM manipulation and for the form validation I used react-hook-form library.",
-    img: "interactive-card-desktop.jpeg",
-    github: "https://github.com/theadusamuel/interactive-card-details-form",
-    liveUrl: "https://interactive-card-details-form-silk.vercel.app",
-    tools: ["react-hook-form"],
-  },
-
-  {
-    id: "06",
-    title: "insure landing page",
-    description:
-      "This is a fully-responsive landing page, built this using the mobile first approach. It's purely HTML and CSS but I used JavaScript for the mobile navigation toggle.",
-    img: "insure-landing-page-desktop.jpeg",
-    github: "https://github.com/theadusamuel/Insure-landing-page",
-    liveUrl: "https://insure-landing-page-nine-drab.vercel.app",
-    tools: ["html", "css"],
-  },
 ];
